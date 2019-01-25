f61a4c089781e4b16d155ad5eb3ea87feedb42af
ferrannp <fnp.developer@gmail.com>
2019-01-25 17:07:11 +0100

    Init: Add Realm

---
+++ App.js
@@ -0,0 +1,52 @@
+// @flow
+
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Realm from 'realm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { realm: null };
+  }
+
+  componentWillMount() {
+    Realm.open({
+      schema: [{ name: 'Dog', properties: { name: 'string' } }],
+    }).then(realm => {
+      realm.write(() => {
+        realm.create('Dog', { name: 'Rex' });
+      });
+      this.setState({ realm });
+    });
+  }
+
+  render() {
+    const info = this.state.realm
+      ? `Number of dogs in this Realm: ${
+          this.state.realm.objects('Dog').length
+        }`
+      : 'Loading...';
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{info}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
